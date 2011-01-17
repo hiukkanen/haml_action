c9dd099e53b7196727f43793ce9ced747b14ad8a
Joni Lahtinen <joni.lahtinen@gmail.com>
2011-01-17 23:22:19 +0200

    Add some haml_action.cgi tests

---
+++ test/test_haml_action.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+load 'haml_action.cgi'
+
+class TestHamlAction < Test::Unit::TestCase
+  def test_should_fail_with_invalid_filename
+    assert_raise Errno::ENOENT do
+      HamlAction::render_haml "invalid"
+    end
+  end
+
+  def test_should_get_correct_output_using_h_and_layout
+    output = HamlAction::render_haml "./testfiles/h_and_layout.haml"
+    expected = "<body>\n  <h1>\n    <p>hi</p>\n    <p>\n      Some paragraph text\n    </p>\n  </h1>\n</body>\n"
+    assert_equal expected, output
+  end
+
+end
